0d6ce5e0dc56193065fd422a2d36fdd1b47808ce
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2020-05-25 18:53:46 +0200

    Oidc provider & test not finished.

--- modules/ty-dao-rdb/src/main/resources/db/migration/y2020/wip_v392__odic.sql
+++ modules/ty-dao-rdb/src/main/resources/db/migration/y2020/wip_v392__odic.sql
@@ -1,6 +1,15 @@
+
+-- Rename to  authn_providers_t  ?
+-- Add  authn_type  OIDC / TwitterOAuth1 / FacebookOAuth2 / ...
+--             no, instead:  OIDC / OAuth1 / OAuth2,  + alias = twitter / facebook / etc
+--                                               + displ name = Twitter / Facebook ...
+--
+-- Then, later on (not now!), multi-site users can config their own FB app for example.
+--
 create table oidc_providers_t(
   site_id_c int not null,
   id_c int not null,
+  authn_type_c,
   alias_c varchar not null,   -- —>   /-/auth/oidc/op_alias  +  /-/auth/oidc/op_alias/callback?code=....
   display_name_c varchar,
   enabled_c bool not null,
@@ -10,14 +19,14 @@ create table oidc_providers_t(
   sync_mode_c int not null,  -- ImportOnFirstLogin  or  SyncOnAllLogins
   op_hosted_domain_c varchar,      -- e.g. Google Gsuite hosted domains
   op_send_user_ip bool,         -- so Google throttles based on the browser's ip instead
-  op_authorization_url_c varchar not null,
-  op_access_token_url_c varchar not null,
-  op_user_info_url_c varchar not null,
+  op_authorization_url_c varchar not null,  -- rename to  oauth2_auth_url ?
+  op_access_token_url_c varchar not null,  -- rename to  oauth2_token_url ?
+  op_user_info_url_c varchar not null,  -- rename to  oauth2_profile_url ?
   op_logout_url_c varchar,
-  op_client_id_c varchar not null,
-  op_client_secret_c varchar not null,
-  op_issuer_c varchar,
-  op_scopes_c varchar,
+  op_client_id_c varchar not null,   -- oauth2_...  ?
+  op_client_secret_c varchar not null,   -- oauth2_...  ?
+  op_issuer_c varchar,   -- oidc_issuer_c  ?
+  op_scopes_c varchar,   -- provider_scopes_c   no  ip_scopes_c ?  identity provider
 
   constraint oidcproviders_p_id primary key (site_id_c, id_c),
 TyE77GJ2
